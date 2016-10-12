7699f0f774c1484fc13f425947ff8cedf54ff6f6
francois <saulnoz@gmail.com>
2016-10-12 16:45:36 +0200

    Initial import

---
+++ src/main/java/fr/blablacar/Greeting.java
@@ -0,0 +1,27 @@
+package fr.blablacar;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Greeting {
+	private static final String TEXT_RESOURCE = "text-file.txt";
+
+	public static void main(String[] args) {
+		System.out.println(new Greeting().getMessage());
+		System.out.println(new Greeting().getMessageFromResource());
+	}
+
+	protected String getMessage() {
+		return "I say Hello for You";
+	}
+
+	protected String getMessageFromResource() {
+		StringBuilder sb = new StringBuilder();
+		InputStream is = Greeting.class.getClassLoader().getResourceAsStream(TEXT_RESOURCE);
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		//br.lines().forEach(line -> sb.append(line));
+		br.lines().forEach(sb::append);
+		return sb.toString();
+	}
+}
---
+++ src/test/java/fr/blablacar/GreetingTest.java
@@ -0,0 +1,25 @@
+package fr.blablacar;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class GreetingTest {
+
+	@Test
+	public void testGetMessage() throws Exception {
+		// Setup
+		Greeting greeting = new Greeting();
+		// Test
+		Assert.assertEquals("I say Hello for You", greeting.getMessage());
+		// Teardown
+	}
+
+	@Test
+	public void testGetMessageFromResource() throws Exception {
+		// Setup
+		Greeting greeting = new Greeting();
+		// Test
+		Assert.assertEquals("I am a text file !!", greeting.getMessageFromResource());
+	}
+
+}
